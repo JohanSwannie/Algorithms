f003e02bf4fa869973e045f8c223b835020a7cee
Johan Swan <jswannieman@gmail.com>
2023-10-04 12:49:11 +1300

    Commit algo Javascript Component

--- algo.js
+++ algo.js
@@ -1,3 +1,13 @@
+const genArray = [];
+const container = document.querySelector(".container");
+
+const generatePtag = (innerContent) => {
+  const pippie = document.createElement("p");
+  pippie.innerHTML = innerContent;
+  container.appendChild(pippie);
+};
+
+//* NUMBER 1
 //* Sum all the numbers in a range with 2 numbers given in or out of sequence
 //* Long way
 
@@ -12,7 +22,11 @@ const sumThemAll1 = (arr1) => {
   return total1;
 };
 
-alert(sumThemAll1([21, 13]));
+const sumAllLong = sumThemAll1([21, 13]);
+
+generatePtag(
+  `Sum all the numbers in a range with 2 numbers given in or out of sequence - Long way &#8594; ${sumAllLong}`
+);
 
 //* Sum all the numbers in a range with 2 numbers given in or out of sequence
 //* Short way
@@ -25,4 +39,48 @@ const sumThemAll2 = (arr2) => {
   return total2;
 };
 
-alert(sumThemAll2([1, 4]));
+const sumAllShort = sumThemAll1([11, 14]);
+
+generatePtag(
+  `Sum all the numbers in a range with 2 numbers given in or out of sequence - Short way &#8594; ${sumAllShort}`
+);
+
+//* NUMBER 2
+//* Return the symmetric differences between 2 arrays
+//* Long way
+
+const symDiff = (arr1, arr2) => {
+  const togetherValues = new Set([...arr1, ...arr2]);
+  const diffArr = [];
+  for (element of togetherValues) {
+    if (arr1.includes(element) && !arr2.includes(element)) {
+      diffArr.push(element);
+    } else if (arr2.includes(element) && !arr1.includes(element)) {
+      diffArr.push(element);
+    }
+  }
+  return diffArr;
+};
+
+const firstArray = [1, 3, 7, 18, 11, 14];
+const secondArray = [2, 3, 18, 10, 7, 11];
+
+const symDifference1 = symDiff(firstArray, secondArray);
+
+generatePtag(
+  `Return the symmetric differences between 2 arrays - Long way &#8594; ${symDifference1}`
+);
+
+//* Return the symmetric differences between 2 arrays
+//* Short way
+
+const arr1 = [9, 13, 3, 17, 10, 11, 29, 18];
+const arr2 = [2, 3, 18, 10, 7, 11, 17];
+
+const diff1 = arr1.filter((item) => !arr2.includes(item));
+const diff2 = arr2.filter((item) => !arr1.includes(item));
+const result = [...diff1, ...diff2];
+
+generatePtag(
+  `Return the symmetric differences between 2 arrays - Long way &#8594; ${result}`
+);
