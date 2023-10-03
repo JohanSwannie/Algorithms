590dccd10e457a0bf1cd709940798b3d47844ef0
Johan Swan <jswannieman@gmail.com>
2023-10-04 11:49:22 +1300

    Commit algo Javascript Component

--- algo.js
+++ algo.js
@@ -1,14 +1,28 @@
 //* Sum all the numbers in a range with 2 numbers given in or out of sequence
+//* Long way
 
-const sumThemAll = (arr) => {
-  arr.sort();
-  let min = arr[0];
-  let max = arr[1];
-  let total = 0;
+const sumThemAll1 = (arr1) => {
+  arr1.sort();
+  let min = arr1[0];
+  let max = arr1[1];
+  let total1 = 0;
   for (let i = min; i <= max; i++) {
-    total += i;
+    total1 += i;
   }
-  return total;
+  return total1;
 };
 
-alert(sumThemAll([21, 13]));
+alert(sumThemAll1([21, 13]));
+
+//* Sum all the numbers in a range with 2 numbers given in or out of sequence
+//* Short way
+
+const sumThemAll2 = (arr2) => {
+  let total2 = 0;
+  for (let i = Math.min(...arr2); i <= Math.max(...arr2); i++) {
+    total2 += i;
+  }
+  return total2;
+};
+
+alert(sumThemAll2([1, 4]));
